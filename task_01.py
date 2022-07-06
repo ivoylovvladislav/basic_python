6ffd9de002bd815d585df859ee25f5eecd750405
Vladislav Ivoilov <1pochta.g@gmail.com>
2022-07-07 00:01:26 +0500

    первые 3 задачи

---
+++ task_01.py
@@ -0,0 +1,10 @@
+number = 157
+word = 'python'
+
+user_name = input('Введите ваше имя: ')
+user_age = int(input('Введите ваше возраст: '))
+
+print('Переменная number =', number)
+print('Переменная worf =', word)
+
+print('Тебя зовут', user_name, 'твой возраст', user_age)
